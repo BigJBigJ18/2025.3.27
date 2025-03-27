8e428c6fc534339a3375f6d25f927fdd87e1f977
BigJBigJ18 <rai240388@spengergasse.at>
2025-03-27 08:51:30 +0100

    EDIT 1: -------------------------------- +Angestellter alle Methoden --------------------------------

--- src/Angestellter.java
+++ src/Angestellter.java
@@ -1,2 +1,58 @@
 public class Angestellter {
+    private String name;
+    private String abteilung;
+    private float gehalt;
+
+    public void setGehalt(float gehalt) {
+        if(gehalt < 0){
+            System.err.println("wrong.angestellter.setname.input.1");
+            gehalt=10000;
+        }
+        this.gehalt = gehalt;
+    }
+
+    public void setAbteilung(String abteilung){
+        if(abteilung.isEmpty()){
+            System.err.println("wrong.angestellter.setname.input.1");
+            abteilung="HIF";
+        }
+        this.abteilung = abteilung;
+    }
+
+    private void setName(String name){
+        if(name.isEmpty()){
+            System.err.println("wrong.angestellter.setname.input.1");
+            name="Martin";
+        }
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbteilung() {
+        return abteilung;
+    }
+
+    public float getGehalt() {
+        return gehalt;
+    }
+
+    public Angestellter(String name, String abteilung, float gehalt){
+        setName(name);
+        setAbteilung(abteilung);
+        setGehalt(gehalt);
+    }
+
+    public boolean gleicheAbteilung(Angestellter ang){
+        if(this.abteilung.equals(ang.getAbteilung())){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return "-----------------------------------------------"+"\nName: "+this.name+"\n Abteilung: "+this.abteilung+"\nGehalt: "+this.gehalt+"\n-----------------------------------------------";
+    }
 }
